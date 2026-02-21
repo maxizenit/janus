a5831daf20ba6d881dd98bab17802cc7fe0f76ad
Maxim Puzanov <maxizenit@gmail.com>
2026-02-22 00:52:08 +0300

    Initial Java SDK annotations

---
+++ sdk/java/annotations/src/main/java/org/janus/sdk/annotation/param/AbsoluteScale.java
@@ -0,0 +1,21 @@
+package org.janus.sdk.annotation.param;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AbsoluteScale {
+
+    String value() default "";
+
+    double min();
+
+    double max();
+
+    Direction direction() default Direction.DECREASE;
+}
