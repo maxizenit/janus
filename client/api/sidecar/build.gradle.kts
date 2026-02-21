b2adab4b3593c6421355a9139b4f66bbac5c3b94
Maxim Puzanov <maxizenit@gmail.com>
2026-02-22 00:21:17 +0300

    Sidecar API gradle module

---
+++ client/api/sidecar/build.gradle.kts
@@ -0,0 +1,40 @@
+import com.google.protobuf.gradle.id
+
+plugins {
+    java
+    id("com.google.protobuf") version libs.versions.protobuf.plugin.get()
+}
+
+group = "org.janus"
+version = "0.0.1-SNAPSHOT"
+description = "Sidecar API"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc") {
+                    option("@generated=omit")
+                }
+            }
+        }
+    }
+}
