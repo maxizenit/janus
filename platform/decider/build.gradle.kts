ebbc7e300f193803895c2605b430cd7df5b146b6
Maxim Puzanov <maxizenit@gmail.com>
2026-02-27 01:49:00 +0300

    Decider stub

---
+++ platform/decider/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+    id("org.springframework.boot") version libs.versions.springBootPlugin.get()
+    id("io.spring.dependency-management") version libs.versions.springDependencyManagementPlugin.get()
+    id("io.freefair.lombok") version libs.versions.lombokPlugin.get()
+}
+
+group = "org.janus"
+version = "0.0.1-SNAPSHOT"
+description = "Decider"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":platform:api:policy-store-api"))
+    implementation(project(":platform:api:state-store-api"))
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.grpc:spring-grpc-spring-boot-starter")
+
+    implementation("io.grpc:grpc-services")
+
+    implementation("org.mapstruct:mapstruct:${libs.versions.mapstruct.get()}")
+    annotationProcessor("org.mapstruct:mapstruct-processor:${libs.versions.mapstruct.get()}")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.grpc:spring-grpc-test")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.grpc:spring-grpc-dependencies:${libs.versions.springGrpc.get()}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ platform/decider/src/main/java/org/janus/decider/configuration/properties/JanusDeciderProperties.java
@@ -0,0 +1,12 @@
+package org.janus.decider.configuration.properties;
+
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.time.DurationMin;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.Duration;
+
+@ConfigurationProperties("janus.decider")
+@Validated
+public record JanusDeciderProperties(@NotNull @DurationMin(millis = 1) Duration policyRefreshInterval) {}
