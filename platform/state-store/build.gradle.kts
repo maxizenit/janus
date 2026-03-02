b04f587bad5c19038cf84bbcf7f07038ae9a8a18
Maxim Puzanov <maxizenit@gmail.com>
2026-03-03 01:52:14 +0300

    State Store Redis configuration

--- platform/state-store/build.gradle.kts
+++ platform/state-store/build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation(project(":platform:api:state-store-api"))
 
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.grpc:spring-grpc-spring-boot-starter")
 
     implementation("io.grpc:grpc-services")
