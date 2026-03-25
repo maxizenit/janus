02859f3d51bd3cd74f31713268898ff0778ea3fc
Maxim Puzanov <maxizenit@gmail.com>
2026-03-25 03:09:36 +0300

    Spring Boot starter autoconfiguration imports

--- sdk/java/spring-boot-starter/src/main/java/org/janus/sdk/starter/configuration/JanusSdkAutoConfiguration.java
+++ sdk/java/spring-boot-starter/src/main/java/org/janus/sdk/starter/configuration/JanusSdkAutoConfiguration.java
@@ -10,18 +10,41 @@ import org.janus.sdk.core.transform.DefaultFallbackArgumentsTransformer;
 import org.janus.sdk.core.transform.FallbackArgumentsTransformer;
 import org.janus.sdk.core.validation.DefaultDegradableDescriptorValidator;
 import org.janus.sdk.core.validation.DegradableDescriptorValidator;
+import org.janus.sdk.starter.aop.DegradableAspect;
+import org.janus.sdk.starter.aop.ReflectionFallbackMethodInvoker;
+import org.janus.sdk.starter.client.GrpcSidecarSdkClient;
 import org.janus.sdk.starter.configuration.properties.JanusSdkProperties;
+import org.janus.sdk.starter.lifecycle.JanusSdkStartupRunner;
+import org.janus.sdk.starter.mapper.SidecarRuntimeStateMapper;
+import org.janus.sdk.starter.registry.RegistryBackedMethodDescriptorResolver;
+import org.janus.sdk.starter.scanner.DegradableDescriptorFactory;
+import org.janus.sdk.starter.scanner.DegradableMethodScanner;
+import org.janus.sdk.starter.scheduling.DegradationRefreshScheduler;
+import org.janus.sdk.starter.service.DegradationRefreshService;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @AutoConfiguration
 @EnableScheduling
 @EnableConfigurationProperties(JanusSdkProperties.class)
 @ConditionalOnProperty(prefix = "janus.sdk", name = "enabled", havingValue = "true")
+@Import({
+        DegradableAspect.class,
+        ReflectionFallbackMethodInvoker.class,
+        RegistryBackedMethodDescriptorResolver.class,
+        DegradableMethodScanner.class,
+        DegradableDescriptorFactory.class,
+        SidecarRuntimeStateMapper.class,
+        GrpcSidecarSdkClient.class,
+        DegradationRefreshService.class,
+        DegradationRefreshScheduler.class,
+        JanusSdkStartupRunner.class
+})
 @NullMarked
 public class JanusSdkAutoConfiguration {
 
