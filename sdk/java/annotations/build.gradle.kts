6b4e525f48e8eafdb5f274eb75514ada0bcad3a8
Maxim Puzanov <maxizenit@gmail.com>
2026-06-01 13:20:46 +0300

    Bump version to 1.3.0

--- sdk/java/annotations/build.gradle.kts
+++ sdk/java/annotations/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "org.janus"
-version = "1.3.0-SNAPSHOT"
+version = "1.3.0"
 description = "Java SDK Annotations"
 
 java {
--- sdk/java/core/build.gradle.kts
+++ sdk/java/core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.janus"
-version = "1.3.0-SNAPSHOT"
+version = "1.3.0"
 description = "Java SDK Core"
 
 java {
